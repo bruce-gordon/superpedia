0df27a5facd927c98402ba6dc2825ca9b83c7da4
Bruce Gordon <brc.gordon@gmail.com>
2021-01-13 17:55:45 -0700

    Set up component files

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <header className='App-header'>
+      <div>
+      <h1>Welcome, Bruce</h1>
+      <p>Pick your character</p>
+      </div>
+    </header>
+  );
+}
+
+export default App;
