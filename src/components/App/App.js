548a503900abf49c3e6ce21745d2cef6c5418313
Bruce Gordon <brc.gordon@gmail.com>
2021-01-14 11:47:42 -0700

    Start Navbar component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,12 +4,13 @@ import './App.css';
 
 const App = () => {
   return (
-    <header className='App-header'>
-      <div>
-      <h1>Welcome, Bruce</h1>
-      <p>Pick your character</p>
-      </div>
-    </header>
+    <main>
+      <header className='App-header'>
+        <div>
+
+        </div>
+      </header>
+    </main>
   );
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navbar.scss';
+
+const Navbar = () => {
+
+}
+
+export default Navbar;
