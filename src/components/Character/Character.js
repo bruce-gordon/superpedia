c79f5521dccd51b7d210bc5c9a21a0b949ff5aa7
Bruce Gordon <brc.gordon@gmail.com>
2021-01-16 13:00:18 -0700

    Style Character component

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -2,11 +2,27 @@ import React from 'react';
 import './Character.scss';
 
 const Character = ({ id, details }) => {
+  const aliases = details.aliases.replaceAll('\r\n', ', ')
+
   return (
-    <article className='character-view'>
+    <section className='character-view'>
       <h1 className='view-header'>{ details.name }
       </h1>
-    </article>
+      <article className='char-bio-area'>
+      <div className='image-div-char'>
+        <img
+          className='image-char'
+          alt={`character-image-${details.name}`}
+          src={details.image.original_url}>
+        </img>
+      </div>
+      <div className='char-info'>
+        <p><b>Real name:</b> { details.real_name }</p>
+        <p><b>Aliases:</b> { aliases }</p>
+
+      </div>
+      </article>
+    </section>
   )
 }
 
