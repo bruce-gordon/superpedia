ba8baea947e017bede20c977e43e4f539422eab6
Bruce Gordon <brc.gordon@gmail.com>
2021-01-17 10:07:17 -0700

    Change rendering so Character is conistent with other components

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -15,55 +15,47 @@ const Character = ({ id, details }) => {
     return charData ? charData.aliases.replaceAll('\r\n', ', ') : '';
   }
 
-  const manageRender = () => {
-    if (charData) {
-      return (
-        <section className='character-view'>
+  return (
+    <section className='character-view'>
+      {!charData &&
+        <h1 className='view-header'>No character to   display --  Please try a new search
+        </h1>}
+      {charData &&
+        <div>
           <h1 className='view-header'>{ charData.name }
           </h1>
           <article className='char-bio-area'>
-          <div className='image-div-char'>
-            <img
-              className='image-char'
-              alt={`${charData.name}`}
-              src={charData.image.original_url}>
-            </img>
-          </div>
-          <div className='char-info'>
-            <p><b>Real name:</b> { charData.real_name }</p>
-            <p><b>Aliases:</b> { formatAliases() }</p>
-            <p><b>Publisher:</b> { charData.publisher.name }</p>
-            <p>{ charData.deck }</p>
-            <div className='link-container'>
-              <a
-                href={ charData.site_detail_url }
-                target="_blank"
-                rel="noreferrer"
-                className='link-text'>
-                <p className='link-to-info'>
-                  More information...
-                </p>
-              </a>
+            <div className='image-div-char'>
+              <img
+                className='image-char'
+                alt={`${charData.name}`}
+                src={charData.image.original_url}>
+              </img>
             </div>
-            <div className='plus-container'>
-            { plus }
+            <div className='char-info'>
+              <p><b>Real name:</b> { charData.real_name }</p>
+              <p><b>Aliases:</b> { formatAliases() }</p>
+              <p><b>Publisher:</b> { charData.publisher.name }</p>
+              <p>{ charData.deck }</p>
+              <div className='link-container'>
+                <a
+                  href={ charData.site_detail_url }
+                  target="_blank"
+                  rel="noreferrer"
+                  className='link-text'>
+                  <p className='link-to-info'>
+                    More information...
+                  </p>
+                </a>
+              </div>
+              <div className='plus-container'>
+              { plus }
+              </div>
             </div>
-          </div>
           </article>
-        </section>
-      )
-    } else {
-      return (
-        <section className='character-view'>
-          <h1 className='view-header'>No character to display --  Please try a new search
-          </h1>
-        </section>
-      )
-    }
-  }
-
-  return (
-    manageRender()
+        </div>
+      }
+      </section>
   )
  }
 
