01dfe4aa13f3d2137cbf1a495168b0ecd95d6d05
Bruce Gordon <brc.gordon@gmail.com>
2021-01-18 11:34:32 -0700

    Test ResultView component

--- src/utilities/fakeData.js
+++ src/utilities/fakeData.js
@@ -23,5 +23,30 @@ export const fakeData = [
       },
       real_name: "Victor Creed",
       site_detail_url: "https://comicvine.gamespot.com/sabretooth/4005-4563/"
-   }
+   },
+   {
+     aliases: "Josh Foley\nJoshua Foley \nGolden Boy\nSlick\nElixir\n",
+     deck: "Elixir was one of the students at Xavier Institute and is known for his ability of biological manipulation which allows him to both restore life or take it on a whim, and is capable of incredible feats being one of the few active and confirmed Omega-level mutants. He is now a part of the New Mutant nation of Krakoa as a member of the Five.",
+     id: 4552,
+     image: {
+        icon_url: "https://comicvine1.cbsistatic.com/uploads/square_avatar/0/4226/131081-172690-elixir.jpg",
+        medium_url: "https://comicvine1.cbsistatic.com/uploads/scale_medium/0/4226/131081-172690-elixir.jpg",
+        screen_url: "https://comicvine1.cbsistatic.com/uploads/screen_medium/0/4226/131081-172690-elixir.jpg",
+        screen_large_url: "https://comicvine1.cbsistatic.com/uploads/screen_kubrick/0/4226/131081-172690-elixir.jpg",
+        small_url: "https://comicvine1.cbsistatic.com/uploads/scale_small/0/4226/131081-172690-elixir.jpg",
+        super_url: "https://comicvine1.cbsistatic.com/uploads/scale_large/0/4226/131081-172690-elixir.jpg",
+        thumb_url: "https://comicvine1.cbsistatic.com/uploads/scale_avatar/0/4226/131081-172690-elixir.jpg",
+        tiny_url: "https://comicvine1.cbsistatic.com/uploads/square_mini/0/4226/131081-172690-elixir.jpg",
+        original_url: "https://comicvine1.cbsistatic.com/uploads/original/0/4226/131081-172690-elixir.jpg",
+        image_tags: "All Images,Earth-616 Elixir"
+    },
+    name: "Elixir",
+    publisher: {
+        api_detail_url: "https://comicvine.gamespot.com/api/publisher/4010-31/",
+        id: 31,
+        name: "Marvel"
+    },
+    real_name: "Joshua Foley",
+    site_detail_url: "https://comicvine.gamespot.com/elixir/4005-4552/"
+}
 ]
