a4d3c533471485b5f7b047004f6094dbfa339900
Bruce Gordon <brc.gordon@gmail.com>
2021-01-18 18:11:12 -0700

    Clear unneeded code

--- src/components/Saved/Saved.js
+++ src/components/Saved/Saved.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { info, plus } from '../../icons/icons.js';
 import './Saved.scss';
