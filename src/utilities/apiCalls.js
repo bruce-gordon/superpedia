e1ad1ea5ebaa009177a4eb23501660626a1a6819
Bruce Gordon <brc.gordon@gmail.com>
2021-01-24 15:46:51 -0700

    Add .env file

--- src/utilities/apiCalls.js
+++ src/utilities/apiCalls.js
@@ -1,11 +1,13 @@
+const API_KEY = process.env.REACT_APP_API_KEY
+
 export const getData = (name) => {
   const proxyUrl = 'https://pure-hollows-05817.herokuapp.com/'
-  return fetch(`${proxyUrl}https://comicvine.gamespot.com/api/characters/?api_key=b75f5fe05e82c4de9fe0cb4d38ed1e69083b81af&filter=name:${name}&field_list=aliases,deck,id,image,name,publisher,real_name,site_detail_url&limit=10&format=json`)
+  return fetch(`${proxyUrl}https://comicvine.gamespot.com/api/characters/?api_key=${API_KEY}&filter=name:${name}&field_list=aliases,deck,id,image,name,publisher,real_name,site_detail_url&limit=10&format=json`)
   .then(resp => resp.json())
 }
 
 export const getCharacterById = (id) => {
   const proxyUrl = 'https://pure-hollows-05817.herokuapp.com/'
-  return fetch(`${proxyUrl}https://comicvine.gamespot.com/api/character/4005-${id}/?api_key=b75f5fe05e82c4de9fe0cb4d38ed1e69083b81af&field_list=aliases,deck,id,image,name,publisher,real_name,site_detail_url&format=json`)
+  return fetch(`${proxyUrl}https://comicvine.gamespot.com/api/character/4005-${id}/?api_key=${API_KEY}&field_list=aliases,deck,id,image,name,publisher,real_name,site_detail_url&format=json`)
   .then(resp => resp.json())
 }
